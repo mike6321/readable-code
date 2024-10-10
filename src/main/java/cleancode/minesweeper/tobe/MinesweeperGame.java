fdea29d61249187deed5184007ae19e9d88bfdf7
mike6321 <mike6321@naver.com>
2024-10-10 09:35:09 +0900

    (refactor) 이름 변경

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -15,9 +15,9 @@ public class MinesweeperGame {
         System.out.println("지뢰찾기 게임 시작!");
         System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 10; j++) {
-                board[i][j] = "□";
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 10; col++) {
+                board[row][col] = "□";
             }
         }
         for (int i = 0; i < 10; i++) {
@@ -61,10 +61,10 @@ public class MinesweeperGame {
         }
         while (true) {
             System.out.println("   a b c d e f g h i j");
-            for (int i = 0; i < 8; i++) {
-                System.out.printf("%d  ", i + 1);
-                for (int j = 0; j < 10; j++) {
-                    System.out.print(board[i][j] + " ");
+            for (int row = 0; row < 8; row++) {
+                System.out.printf("%d  ", row + 1);
+                for (int col = 0; col < 10; col++) {
+                    System.out.print(board[row][col] + " ");
                 }
                 System.out.println();
             }
@@ -78,54 +78,54 @@ public class MinesweeperGame {
             }
             System.out.println();
             System.out.println("선택할 좌표를 입력하세요. (예: a1)");
-            String input = scanner.nextLine();
+            String cellInput = scanner.nextLine();
             System.out.println("선택한 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
-            String input2 = scanner.nextLine();
-            char c = input.charAt(0);
-            char r = input.charAt(1);
-            int col;
-            switch (c) {
+            String userInput = scanner.nextLine();
+            char cellInputCol = cellInput.charAt(0);
+            char cellInputRow = cellInput.charAt(1);
+            int selectedColIndex;
+            switch (cellInputCol) {
                 case 'a':
-                    col = 0;
+                    selectedColIndex = 0;
                     break;
                 case 'b':
-                    col = 1;
+                    selectedColIndex = 1;
                     break;
                 case 'c':
-                    col = 2;
+                    selectedColIndex = 2;
                     break;
                 case 'd':
-                    col = 3;
+                    selectedColIndex = 3;
                     break;
                 case 'e':
-                    col = 4;
+                    selectedColIndex = 4;
                     break;
                 case 'f':
-                    col = 5;
+                    selectedColIndex = 5;
                     break;
                 case 'g':
-                    col = 6;
+                    selectedColIndex = 6;
                     break;
                 case 'h':
-                    col = 7;
+                    selectedColIndex = 7;
                     break;
                 case 'i':
-                    col = 8;
+                    selectedColIndex = 8;
                     break;
                 case 'j':
-                    col = 9;
+                    selectedColIndex = 9;
                     break;
                 default:
-                    col = -1;
+                    selectedColIndex = -1;
                     break;
             }
-            int row = Character.getNumericValue(r) - 1;
-            if (input2.equals("2")) {
-                board[row][col] = "⚑";
+            int selectedRowIndex  = Character.getNumericValue(cellInputRow) - 1;
+            if (userInput.equals("2")) {
+                board[selectedRowIndex][selectedColIndex] = "⚑";
                 boolean open = true;
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        if (board[i][j].equals("□")) {
+                for (int row = 0; row < 8; row++) {
+                    for (int col = 0; col < 10; col++) {
+                        if (board[row][col].equals("□")) {
                             open = false;
                         }
                     }
@@ -133,23 +133,23 @@ public class MinesweeperGame {
                 if (open) {
                     gameStatus = 1;
                 }
-            } else if (input2.equals("1")) {
-                if (landMines[row][col]) {
-                    board[row][col] = "☼";
+            } else if (userInput.equals("1")) {
+                if (landMines[selectedRowIndex][selectedColIndex]) {
+                    board[selectedRowIndex][selectedColIndex] = "☼";
                     gameStatus = -1;
                     continue;
                 } else {
-                    open(row, col);
+                    open(selectedRowIndex, selectedColIndex);
                 }
-                boolean open = true;
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        if (board[i][j].equals("□")) {
-                            open = false;
+                boolean isAllOpened = true;
+                for (int row = 0; row < 8; row++) {
+                    for (int col = 0; col < 10; col++) {
+                        if (board[row][col].equals("□")) {
+                            isAllOpened = false;
                         }
                     }
                 }
-                if (open) {
+                if (isAllOpened) {
                     gameStatus = 1;
                 }
             } else {
@@ -158,7 +158,7 @@ public class MinesweeperGame {
         }
     }
 
-    private static void open(int row, int col) {
+    private static void  open(int row, int col) {
         if (row < 0 || row >= 8 || col < 0 || col >= 10) {
             return;
         }
