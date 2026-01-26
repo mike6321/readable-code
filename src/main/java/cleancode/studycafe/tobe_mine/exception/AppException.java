37db3ef04ac4a829bd8c3fa2624497ec8bbec760
mike6321 <mike6321@naver.com>
2026-01-26 09:34:23 +0900

    (refactor) reset

--- src/main/java/cleancode/studycafe/tobe_mine/exception/AppException.java
+++
@@ -1,9 +0,0 @@
-package cleancode.studycafe.tobe_mine.exception;
-
-public class AppException extends RuntimeException {
-
-    public AppException(String message) {
-        super(message);
-    }
-
-}
