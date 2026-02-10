26044a9cf73aa688843c364b6fda9735690a9d9e
mike6321 <mike6321@naver.com>
2026-02-10 21:21:35 +0900

    feature: ValueObject#2

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
@@ -1,14 +1,28 @@
 package cleancode.minesweeper.tobe.io;
 
+import cleancode.minesweeper.tobe.BoardIndexConverter;
+import cleancode.minesweeper.tobe.position.CellPosition;
+
 import java.util.Scanner;
 
 public class ConsoleInputHandler implements InputHandler {
+
+    private final BoardIndexConverter boardIndexConverter = new BoardIndexConverter();
 
     public static final Scanner SCANNER = new Scanner(System.in);
 
     @Override
     public String getUserInput() {
         return SCANNER.nextLine();
+    }
+
+    @Override
+    public CellPosition getCellPositionFromUser() {
+        String userInput = SCANNER.nextLine();
+        int columnIndex = this.boardIndexConverter.getSelectedColumnIndex(userInput);
+        int rowIndex = this.boardIndexConverter.getSelectedRowIndex(userInput);
+
+        return CellPosition.of(rowIndex, columnIndex);
     }
 
 }
