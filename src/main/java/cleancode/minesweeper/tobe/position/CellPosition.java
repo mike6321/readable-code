5781b535cf87ad3e64c5dc2d5963638479630825
mike6321 <mike6321@naver.com>
2026-02-10 09:44:48 +0900

    feature: ValueObject#1

---
+++ src/main/java/cleancode/minesweeper/tobe/position/CellPosition.java
@@ -0,0 +1,34 @@
+package cleancode.minesweeper.tobe.position;
+
+import java.util.Objects;
+
+public class CellPosition {
+
+    private final int rowIndex;
+    private final int columnIndex;
+
+    public CellPosition(int rowIndex, int columnIndex) {
+        if (rowIndex < 0 || columnIndex < 0) {
+            throw new IllegalArgumentException("올바르지 않은 좌표입니다.");
+        }
+        this.rowIndex = rowIndex;
+        this.columnIndex = columnIndex;
+    }
+
+    public static CellPosition of(int rowIndex, int columnIndex) {
+        return new CellPosition(rowIndex, columnIndex);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CellPosition that = (CellPosition) o;
+        return rowIndex == that.rowIndex && columnIndex == that.columnIndex;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rowIndex, columnIndex);
+    }
+
+}
